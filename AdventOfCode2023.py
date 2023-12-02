783dde208d3a2db09039cb14e785be9738279df2
sakethnadiger <saketh.nadiger@gmail.com>
2023-12-02 06:17:03 +0000

    day 2 completed

--- AdventOfCode2023.py
+++ AdventOfCode2023.py
@@ -52,3 +52,72 @@ with open("myfile.txt", "r") as f:
 
 
 #DAY 2
+# totalID = 0
+
+# powerTotal = 0
+# games = []
+# for x in range(1, 101):
+#   games.append(x)
+
+# for i in contents:
+#   minSet = []
+#   maxGreen = 0
+#   maxRed = 0
+#   maxBlue = 0
+#   power = 1
+#   lenGameSubstring = i.find(':')
+#   if lenGameSubstring == 6:
+#     gameNumber = int(i[5])
+#   elif lenGameSubstring == 7:
+#     gameNumber = int(i[5] + i[6])
+#   elif lenGameSubstring == 8:
+#     gameNumber = int(i[5] + i[6] + i[7])
+    
+#   else:
+#     continue
+  
+  
+#   sets = i.split(';')
+#   firstSet = sets[0].split(':')
+#   del firstSet[0]
+#   del sets[0]
+#   sets.append(''.join(firstSet))
+#   for j in sets:
+#     contents = j.split(',')
+#     for item in contents:
+#       colourAndNum = item.split(' ')
+#       numColours = int(colourAndNum[1])
+#       colour = colourAndNum[2]
+#       if colour == 'red':
+#         if numColours >= maxRed:
+#           maxRed = numColours
+#         # if numColours > 12:
+#         #   if gameNumber in games:
+#         #     games.remove(gameNumber)
+#         # else:
+#         #   continue
+        
+#       elif colour == 'green':
+#         if numColours >= maxGreen:
+#           maxGreen = numColours
+#         # if numColours > 13:
+#         #   if gameNumber in games:
+#         #     games.remove(gameNumber)
+#       elif colour == 'blue':
+#         if numColours >= maxBlue:
+#           maxBlue = numColours
+#         # if numColours > 14:
+#         #   if gameNumber in games:
+#         #     games.remove(gameNumber)
+#       else:
+#         continue
+#   max = [maxBlue, maxGreen, maxRed]
+#   minSet.extend(max)
+#   for m in max:
+#     power = power * m
+#   powerTotal += power
+
+# for y in games:
+#   totalID += y
+
+#DAY 3
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,2 @@
-else:
-      #   temp = ''
-      #   continue
+
+  if possible:
